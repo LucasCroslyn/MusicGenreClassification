e8104a5e02d2e35816bd1933385ada53a3fe1449
Lucas Croslyn <croslynlc@gmail.com>
2025-05-08 21:57:42 -0300

    Optimized LSTM

--- src/LSTM.py
+++ src/LSTM.py
@@ -1,7 +1,8 @@
+import numpy as np
 import keras
 import keras.layers as layers
-import numpy as np
 from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.regularizers import l2
 
 
 def read_model(model_file: str = None):
@@ -28,17 +29,18 @@ class LSTM:
         if self.model is None:
             model = keras.Sequential()
             model.add(layers.Input(shape=(train_x.shape[1], train_x.shape[2])))
-            model.add(layers.LSTM(units=128, dropout=0.3, recurrent_dropout=0.3, return_sequences=True))
+            model.add(layers.Bidirectional(layers.LSTM(units=128, dropout=0.3, kernel_regularizer=l2(0.01), return_sequences=True)))
             model.add(layers.BatchNormalization())
-            model.add(layers.LSTM(units=64, dropout=0.3, recurrent_dropout=0.3))
+            model.add(layers.Bidirectional(layers.LSTM(units=64, dropout=0.3, kernel_regularizer=l2(0.01))))
             model.add(layers.BatchNormalization())
-            model.add(layers.Flatten())
-            model.add(layers.Dense(128, activation="relu"))
+            model.add(layers.Dense(128, activation="relu", kernel_regularizer=l2(0.01)))
+            model.add(layers.Dropout(0.3))
             model.add(layers.Dense(units=train_y.shape[1], activation="softmax"))
             model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+            model.summary()
             self.model = model
 
-        early_stopping = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=5)
+        # early_stopping = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=5)
 
         if model_file is not None:
             model_checkpoint = ModelCheckpoint(model_file, monitor='val_loss', mode='min', save_best_only=True)
@@ -51,7 +53,8 @@ class LSTM:
                                       steps_per_epoch=steps_per_epoch,
                                       verbose=1,
                                       use_multiprocessing=True,
-                                      callbacks=[early_stopping, model_checkpoint])
+                                    #   callbacks=[early_stopping, model_checkpoint])
+                                      callbacks=[model_checkpoint])
 
     def evaluate(self, test_x: np.ndarray, test_y: np.ndarray, batch_size: int = 10) -> float:
         return self.model.evaluate(test_x, test_y, batch_size=batch_size)
